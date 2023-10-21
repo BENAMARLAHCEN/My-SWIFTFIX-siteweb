e7629ae690cde16cd1dc2fb060eb4f1c220f8fd1
lahcen <lahcen.ben3@gmail.com>
2023-10-21 09:34:20 +0100

    create js fille and add dropdown menu to navbar

---
+++ svript.js
@@ -0,0 +1,33 @@
+const toggleButton = document.getElementsByClassName('menu')[0]
+const navbar = document.getElementsByClassName('navbar')[0]
+console.log(navbar)
+console.log(toggleButton)
+toggleButton.addEventListener('click', () => {
+    navbar.classList.toggle('red')
+})
+
+
+
+
+
+// const list = document.getElementsByClassName('list');
+// const li = createElement('li'); 
+// console.log(li);
+// const li = document.getElementsByName('text');
+// console.log(li);
+// const li = document.querySelector(".item");
+
+
+// li.classList.add("red")// add style
+//li.classList.remove("red")//remove style
+
+// li.id = "class"//add id or change
+
+// const attribute = li.setAttribute("id" , "nime")
+// console.log(attribute)
+// console.log(li); 
+
+// const btn =document.getElementsByClassName("button")[0]
+// btn.addEventListener("click", () => {
+//          li.classList.toggle("red")
+// })
