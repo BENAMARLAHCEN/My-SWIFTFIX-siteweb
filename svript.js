1a793721a9ba0d0e4695ca0767ac1cdad25db808
lahcen <lahcen.ben3@gmail.com>
2023-10-23 14:26:57 +0100

    edit FAQ section action open and close ansre

--- svript.js
+++ svript.js
@@ -1,3 +1,4 @@
+// menu dropdown
 const toggleButton = document.getElementsByClassName('menu')[0]
 const navbar = document.getElementsByClassName('navbar')[0]
 
@@ -5,14 +6,33 @@ toggleButton.addEventListener('click', () => {
     navbar.classList.toggle('red')
 })
 
-const faqs = document.querySelectorAll('.faq');
-faqs.forEach(faq => {
-    faq.addEventListener('click', () => {
-        faq.classList.toggle('active')
+// FAQ affiche
+// const faqs = document.querySelectorAll('.faq');
+// faqs.forEach(faq => {
+//     faq.addEventListener('click', () => {
+//         faq.classList.toggle('active')
+//     })
+
+// })
+
+const quistion = document.querySelectorAll('.quistion')
+const faq = document.querySelectorAll('.faq')
+for (let i=0 ; i<quistion.length;i++){
+    quistion[i].addEventListener('click', function () {
+        faq[i].classList.toggle('active')
+        for (let g = 0; g < quistion.length; g++) {
+        if (g != i) {
+            faq[g].classList.remove('active')
+        }
+    }
     })
+    
+}
 
-})
 
+
+
+// search
 const search = document.getElementsByClassName('search')[0]
 const search_icon = document.getElementsByClassName('search_icon')[0]
 
@@ -22,7 +42,7 @@ search_icon.addEventListener('click', () => {
 })
 
 
-
+// footer 
 const p = document.querySelectorAll('.list_title');
 const ul = document.querySelectorAll('.footer_section .link .ul');
 for (let i = 0; i < p.length; i++) {
@@ -44,18 +64,55 @@ for (let i = 0; i < p.length; i++) {
     })
 }
 
+// const openMore = document.querySelectorAll('.open_more');
+// const modalService = document.querySelectorAll('.modal-service');
+// const closeSer = document.querySelectorAll('.close_ser');
+// console.log(modalService)
+// for(let f=0 ; f< openMore.length ; f++){
+//     openMore[f].addEventListener('click', () => {
+//         modalService[f].classList.add('open_model')
+//     })
+    
+// }
+// for(let f=0 ; f< closeSer.length ; f++){
+//     closeSer[f].addEventListener('click', () => {
+//         modalService[f].classList.remove('open_model')
+//     })
+// }
 
 
 
-const open = document.querySelectorAll('.open')[0]
-const modal = document.querySelectorAll('.modal')[0]
-const close = document.querySelectorAll('.close')[0]
-open.addEventListener('click', () => {
-    modal.classList.add('open_model')
-})
-close.addEventListener('click', () => {
-    modal.classList.remove('open_model')
-})
+
+// const open = document.querySelectorAll('.open')[0]
+// const modal = document.querySelectorAll('.modal')[0]
+// const close = document.querySelectorAll('.close')[0]
+// open.addEventListener('click', () => {
+//     modal.classList.add('open_model')
+// })
+// close.addEventListener('click', () => {
+//     modal.classList.remove('open_model')
+// })
+
+
+// modal 
+const openMore = document.querySelectorAll('.open');
+const modalService = document.querySelectorAll('.modal');
+const closeSer = document.querySelectorAll('.close');
+console.log(modalService)
+for(let f=0 ; f< openMore.length ; f++){
+    openMore[f].addEventListener('click', () => {
+        modalService[f].classList.add('open_model')
+    })
+    
+}
+for(let f=0 ; f< closeSer.length ; f++){
+    closeSer[f].addEventListener('click', () => {
+        modalService[f].classList.remove('open_model')
+    })
+}
+
+
+
 
 
 
