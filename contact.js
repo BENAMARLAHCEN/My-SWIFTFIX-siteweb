5a28f384b363a85634c5efcb60ba2e413a2155d9
lahcen <lahcen.ben3@gmail.com>
2023-10-26 15:12:06 +0100

    quelque modification dans le style de contact validation

--- contact.js
+++ contact.js
@@ -25,29 +25,44 @@ function validateForm() {
         const nameError = document.querySelector('.name-error');
         nameError.classList.add('error')
         name.classList.add('inputerror')
+        name.classList.remove('inputvalid')
+
         valid = false;
+    }else{
+        name.classList.add('inputvalid')
     }
     const phoneRegex = /\+212\-+[6|7]+\d{8}$/;
     if (!phoneRegex.test(phoneNumber.value)) {
         const phoneError = document.querySelector('.phone-error');
         phoneError.classList.add('error');
         phoneNumber.classList.add('inputerror')
-        valid = false;
-    }
+        phoneNumber.classList.remove('inputvalid')
 
+        valid = false;
+    }else{
+        phoneNumber.classList.add('inputvalid')
+    }
     const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
     if (!emailRegex.test(email.value)) {
         const emailError = document.querySelector('.email-error');
         emailError.classList.add('error');
         email.classList.add('inputerror')
+        email.classList.remove('inputvalid')
+
         valid = false;
+    }else{
+        email.classList.add('inputvalid')
     }
 
     if (message.value === '') {
         const messageError = document.querySelector('.message-error');
         messageError.classList.add('error');
         message.classList.add('inputerror')
+        message.classList.remove('inputvalid')
+
         valid = false;
+    }else{
+        message.classList.add('inputvalid')
     }
 
     return valid;
